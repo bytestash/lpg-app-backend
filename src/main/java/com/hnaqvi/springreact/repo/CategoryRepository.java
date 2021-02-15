dd815eee54ebadd12ea86e31b3122eede23ee02d
bytestash <code@hnaqvi.com>
2021-02-15 01:08:15 +0000

    allow cross origin resource

--- src/main/java/com/hnaqvi/springreact/repo/CategoryRepository.java
+++ src/main/java/com/hnaqvi/springreact/repo/CategoryRepository.java
@@ -2,10 +2,12 @@ package com.hnaqvi.springreact.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.hnaqvi.springreact.dbo.Category;
 
 @RepositoryRestResource(collectionResourceRel = "categories", path = "category")
+@CrossOrigin
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- src/main/java/com/hnaqvi/springreact/repo/ProductRepository.java
+++ src/main/java/com/hnaqvi/springreact/repo/ProductRepository.java
@@ -2,10 +2,12 @@ package com.hnaqvi.springreact.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.hnaqvi.springreact.dbo.Product;
 
 @RepositoryRestResource(collectionResourceRel = "products", path = "product")
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
