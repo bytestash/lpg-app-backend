2fbbed44a627041589cc8446b6f8510a52f5f982
bytestash <code@hnaqvi.com>
2021-02-15 21:01:58 +0000

    add category view

--- src/main/java/com/hnaqvi/springreact/dbo/view/CategoryView.java
+++ src/main/java/com/hnaqvi/springreact/dbo/view/CategoryView.java
@@ -1,26 +1,16 @@
 package com.hnaqvi.springreact.dbo.view;
 
-
-import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
 import com.hnaqvi.springreact.dbo.Category;
-import com.hnaqvi.springreact.dbo.Product;
 
-@Projection(name = "productView", types = Product.class)
-public interface ProductView {
+@Projection(name = "categoryView", types = Category.class)
+public interface CategoryView {
 
 	@Value("#{target.id}")
 	String getId();
 
 	String getName();
-
-	String getDescription();
-
-	LocalDateTime getLastPurchasedDate();
-
-	Category getCategory();
 
 }
--- src/main/java/com/hnaqvi/springreact/repo/CategoryRepository.java
+++ src/main/java/com/hnaqvi/springreact/repo/CategoryRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.hnaqvi.springreact.dbo.Category;
+import com.hnaqvi.springreact.dbo.view.CategoryView;
+import com.hnaqvi.springreact.dbo.view.ProductView;
 
-@RepositoryRestResource(collectionResourceRel = "categories", path = "category")
+@RepositoryRestResource(collectionResourceRel = "categories", path = "category", excerptProjection = CategoryView.class)
 @CrossOrigin
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
